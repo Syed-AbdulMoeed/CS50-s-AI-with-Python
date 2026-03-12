e907fa6d81e9fbf553de6aeba01d92cdfc0b592d
Syed-AbdulMoeed <bronzesheild2@gmail.com>
2026-03-12 12:26:24 +0300

    Implemented result()

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -52,14 +53,28 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    if type(action) != 'tuple'  or type(action[1]) != 'int' or type(action[0] != 'int'):
+        raise TypeError("tuple of ints")
+    if action[0] < 0 or action[0] > 2 or action[1] < 0 or action[1] > 2:
+        raise IndexError("Index value out of bounds")
+    
+    temp = copy.deepcopy(board)
+    player = player(board)
+    temp[action[0]][action[1]] = player
+    return temp
+    
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+
+    for player in [X, O]:
+        if (any(all(board[i][j] == player for j in range(3)) for i in range(3)) or # check horizontal
+            any(all(board[i][j] == player for i in range(3)) for j in range(3)) or # check vertically
+            (board[1][1] == player and ((board[0][0] == player and board[2][2] == player) or (board[0][2] == player and board[2][0] == player)))):
+            return player
+    return None
 
 
 def terminal(board):
